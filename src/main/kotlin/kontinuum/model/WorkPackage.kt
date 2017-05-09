43547ad1acbb987bc4fc4111c60b0e9032ef1ff5
ligi <ligi@ligi.de>
2017-05-09 16:50:24 +0200

    Add time information and CommitMessage+Branch

--- src/main/kotlin/kontinuum/model/WorkPackage.kt
+++ src/main/kotlin/kontinuum/model/WorkPackage.kt
@@ -2,5 +2,8 @@ package kontinuum.model
 
 data class WorkPackage(val project: String,
                        val commitHash: String,
+                       val epochSeconds: Long,
+                       var commitMessage: String? = null,
+                       var branch: String? = null,
                        var workPackageStatus: WorkPackageStatus,
                        var stageInfoList: MutableList<StageInfo> = mutableListOf())
