1b2dc5b930fee52b9aa156d40a453cb47512a3d2
ligi <ligi@ligi.de>
2017-05-10 12:19:14 +0200

    Add installation-id

--- src/main/kotlin/kontinuum/model/WorkPackage.kt
+++ src/main/kotlin/kontinuum/model/WorkPackage.kt
@@ -3,6 +3,8 @@ package kontinuum.model
 data class WorkPackage(val project: String,
                        val commitHash: String,
                        val epochSeconds: Long,
+                       val installationId: String,
+
                        var commitMessage: String? = null,
                        var branch: String? = null,
                        var workPackageStatus: WorkPackageStatus,
